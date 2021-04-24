2db73de714489e30328beca43e94d23dc2d7a425
bipinparajuli <bipinprjl@gmail.com>
2021-04-24 06:30:07 +0545

    callback hell

--- app.js
+++ app.js
@@ -14,3 +14,16 @@
 // // console.log(fun)
 
 // console.log("Hello")
+
+function delay (callback,s){
+    setTimeout(callback,s*1000)
+}
+
+console.log("Starting delay");
+
+delay(()=>{
+    console.log('after 2 second')
+    delay(()=>{
+        console.log("After 1 second")
+    },1)
+},2)
